9e83fa9124267492a750b2244f3272bf89308ada
esteniovasc <esteniovasconcelos@gmail.com>
2026-01-21 01:04:48 -0300

    Adiciona e utiliza ícones de ponteiro alternativos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			cursor: {
+				'pd-default': 'url(/cursors/default.svg) 0 0, auto',
+				'pd-pointer': 'url(/cursors/pointer.svg) 6 0, pointer',
+				'pd-text': 'url(/cursors/text.svg) 12 12, text',
+				'pd-grab': 'url(/cursors/grab.svg) 12 12, grab',
+				'pd-grabbing': 'url(/cursors/grabbing.svg) 12 12, grabbing',
+			},
+		},
+	},
+	plugins: [],
 }
